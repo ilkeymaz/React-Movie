4df1bc8946d6cd993962f80a5f6c4a744062ba13
İlke Yılmaz <107076639+ilkeymaz@users.noreply.github.com>
2025-03-26 22:46:12 +0300

    Update MovieCard.jsx

--- at/src/components/MovieCard.jsx
+++ at/src/components/MovieCard.jsx
@@ -1,3 +1,4 @@
+import '../css/MovieCard.css'
 function MovieCard({movie}){
 
     function onHeartClick(){
@@ -6,15 +7,17 @@ function MovieCard({movie}){
 
     return <div className= "movie-card" >
         <div className="movie-poster">
-            <img src={"movie.url"} alt="movie.title"/>
-            <div className="movie.overlay">
-                <button className="favourite-button" onClick={onHeartClick}> 
-                    ♥ 
+            
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="movie.title"/>
+            
+            
+            <button className="favourite-button" onClick={onHeartClick}> 
+                    FAV BUTON 
                 </button>
-            </div>
             <div className="movie-info">
                 <h3> {movie.title}</h3>
-                <p>{movie.release_date}</p>
+                <p>{movie.release_date?.split("-")[0]}</p> //splitting from the first - to not show the month and day
+                
 
             </div>
         </div>
